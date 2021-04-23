6376f0e5d1ea27af1f3f19035f866c33cf996ffe
Firas <f.sahli@quict.nl>
2021-04-23 09:33:52 +0200

    add a customer model seeder
    Signed-off-by: Firas <f.sahli@quict.nl>

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('customers')->insert([
+          'name' => 'UMC Utrecht',
+      ]);
+
+      DB::table('customers')->insert([
+          'name' => 'AMC',
+      ]);
+    }
+}
