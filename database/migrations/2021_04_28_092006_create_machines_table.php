ea4d1a8c2e55ae38144a15b74927c6c85e15af7a
Firas <f.sahli@quict.nl>
2021-05-02 20:50:35 +0200

    Add a migrfation for machine table

---
+++ database/migrations/2021_04_28_092006_create_machines_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMachinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('machines', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('number')->required()->unique();
+            $table->string('machine_id')->required()->unique();
+            $table->string('type_serial')->required()->unique();
+            $table->dateTime('online_from')->required()->default(DB::raw('CURRENT_TIMESTAMP'));
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('machines');
+    }
+}
